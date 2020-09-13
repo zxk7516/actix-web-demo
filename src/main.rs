98821fa88a3a51953b5f368d7026b2a3a5df93b9
zxk7516 <zxk7516@foxmail.com>
2020-09-13 09:57:36 +0800

    *

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate diesel;
+use actix_web::{web, App, HttpServer};
+
+mod models;
+mod routes;
+mod schema;
+
+use diesel::{
+    r2d2::{self, ConnectionManager},
+    SqliteConnection,
+};
+
+pub type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    let database_url: String = std::env::var("DATABASE_URL").expect("DATABASE_URL not found");
+    let database_pool = Pool::builder()
+        .build(ConnectionManager::<SqliteConnection>::new(database_url))
+        .expect("database pool failed");
+
+    HttpServer::new(move || {
+        App::new()
+            .data(database_pool.clone())
+            .route("/", web::get().to(routes::home))
+            .route("/add_link", web::post().to(routes::add_link))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
